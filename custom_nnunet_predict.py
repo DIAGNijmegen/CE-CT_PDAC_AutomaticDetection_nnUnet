cf6737c0c435203b7fe57746042796be52f5f494
NataliaAlves13 <50765571+NataliaAlves13@users.noreply.github.com>
2023-07-18 11:29:32 +0100

    add license

--- custom_nnunet_predict.py
+++ custom_nnunet_predict.py
@@ -1,6 +1,18 @@
-"""This script contains a customized inference pipeline for nnunet,
-optimized for the specific use case of the FLARE22 challeng.
-"""
+#Copyright 2023 Diagnostic Image Analysis Group, Radboud
+#
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+
+
 import os
 import argparse
 from copy import deepcopy
